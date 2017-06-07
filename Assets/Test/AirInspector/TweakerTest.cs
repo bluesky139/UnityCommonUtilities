66b5f3779bc849e6c505e4ef1ae3ca414f468cf1
bluesky139 <bluesky139@gmail.com>
2017-06-07 16:11:57 +0800

    Test air tweaker.

---
+++ Assets/Test/AirInspector/TweakerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AirInspector;
+
+class TweakerTest : AirTweaker
+{
+	protected override Dictionary<string, object> GetSerializeObjs()
+	{
+		var objs = new Dictionary<string, object>();
+		var behaviour = GetComponent<TweakerTestBehaviour>();
+		objs.Add("maxNum", behaviour.maxNum);
+		objs.Add("maxFloat", behaviour.maxFloat);
+		objs.Add("aString", behaviour.aString);
+		return objs;
+	}
+
+	protected override void SetFromSerializedObjs(Dictionary<string, object> objs)
+	{
+		var behaviour = GetComponent<TweakerTestBehaviour>();
+		foreach (var kv in objs)
+		{
+			if (kv.Key == "maxNum")
+			{
+				behaviour.maxNum = (int) kv.Value;
+			}
+			else if (kv.Key == "maxFloat")
+			{
+				behaviour.maxFloat = (float) kv.Value;
+			} 
+			else if (kv.Key == "aString")
+			{
+				behaviour.aString = (string) kv.Value;
+			}
+		}
+	}
+}
---
+++ Assets/Test/AirInspector/TweakerTestBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class TweakerTestBehaviour : MonoBehaviour
+{
+	private int maxNum_ = 7;
+	private float maxFloat_ = 29.9f;
+	private string aString_ = "a value";
+
+	public int maxNum
+	{
+		get
+		{
+			return maxNum_;
+		}
+		set
+		{
+			Debug.Log("TweakerTestBehaviour set maxNum " + value);
+			maxNum_ = value;
+		}
+	}
+
+	public float maxFloat
+	{
+		get
+		{
+			return maxFloat_;
+		}
+		set
+		{
+			Debug.Log("TweakerTestBehaviour set maxFloat " + value);
+			maxFloat_ = value;
+		}
+	}
+
+	public string aString
+	{
+		get
+		{
+			return aString_;
+		}
+		set
+		{
+			Debug.Log("TweakerTestBehaviour set aString " + value);
+			aString_ = value;
+		}
+	}
+}
